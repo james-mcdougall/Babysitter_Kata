21178b19c83ee986157c39534a73e5d7ec54189b
james-mcdougall <jameswilliammcdougall@gmail.com>
2023-01-23 15:57:53 -0600

    Changed to SwiftUI. Initial Project Commit

---
+++ BabySitterKata/BabySitterKata/BabySitterKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySitterKataApp.swift
+//  BabySitterKata
+//
+//  Created by James McDougall on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct BabySitterKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabySitterKata/BabySitterKata/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BabySitterKata
+//
+//  Created by James McDougall on 1/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
