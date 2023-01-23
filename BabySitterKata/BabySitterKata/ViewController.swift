ed3d94344806ccf19aa4ab93e1f14d9004e9fa75
james-mcdougall <jameswilliammcdougall@gmail.com>
2023-01-23 15:15:58 -0600

    Initial project commit

---
+++ BabySitterKata/BabySitterKata/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  BabySitterKata
+//
+//  Created by James McDougall on 1/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var preBedtimeButton: UIButton!
+    @IBOutlet weak var bedtimeButton: UIButton!
+    @IBOutlet weak var midnightButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func preBedtimeButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func bedTimeButonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func midnightButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+}
+
